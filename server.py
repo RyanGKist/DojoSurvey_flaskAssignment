b2aba2ef1d1c850d5803fc7481be65cc8b2375be
Ryan Kistner <RyanGKistner@Patricks-MacBook-Pro-2.local>
2017-11-04 11:27:38 -0700

    Validation for radio needs work

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,flash,redirect,session
 app = Flask(__name__)
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+app.secret_key="ThisisSecret"
 
 @app.route('/')
 def index():
@@ -7,9 +11,31 @@ def index():
 
 @app.route('/process', methods=['POST'] )
 def process():
-	name = request.form['name']
-	email = request.form['email']
-	number = request.form['number']
-	location = request.form['location']
-	return render_template("process.html",name=name,email=email,number=number,location=location)
+	session['name'] = request.form['name']
+	session['email'] = request.form['email']
+	session['number'] = request.form['number']
+	session['location'] = request.form['location']
+
+	errors = False
+
+	if len(request.form['name']) < 1:
+		flash("Name can not be empty")
+		errors = True
+
+	if not EMAIL_REGEX.match(request.form['email']):
+		flash("Email is incorrect")
+		errors = True
+
+	if len(request.form['number']) < 1:
+		flash("Please add a phone number")
+		errors = True
+	if len(request.form['location']) < 1:
+		flash("please select a location")
+		errors = True
+	
+	if errors:
+		return redirect('/')
+	else:
+		return render_template('process.html')
+
 app.run(debug=True)	
